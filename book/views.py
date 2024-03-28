794d6b944a6c424dd97dd62b278d32b908db62e9
2017Pro <adiletmamajanov9@gmail.com>
2024-03-27 18:23:06 -0700

    commi

--- book/views.py
+++ book/views.py
@@ -26,16 +26,31 @@ class BookDetailView(generic.DetailView):
 #         query_id = get_object_or_404(Book, id=id)
 #         return render(request, template_name='books_detail.html', context={'query_id': query_id})
 
-
 class SearchView(generic.ListView):
     template_name = 'books.html'
-    context_object_name = 'books'
-    paginate_by = 5
+    context_object_name = 'book'
+    paginate_by = '3'
 
     def get_queryset(self):
-        return Book.objects.filter(title__icontains=self.request.GET.get('q')).order_by('id')
+        query_param = self.request.GET.get('q')
+        if query_param is not None:
+            return Book.objects.filter(title__icontains=query_param)
+        else:
+            return Book.objects.none()
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        contex = super().get_context_data(**kwargs)
-        contex['q'] = self.request.GET.get('q')
-        return contex
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
+# class SearchView(generic.ListView):
+#     template_name = 'books.html'
+#     context_object_name = 'books'
+#     paginate_by = 5
+#
+#     def get_queryset(self):
+#         return Book.objects.filter(title__icontains=self.request.GET.get('q')).order_by('id')
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         contex = super().get_context_data(**kwargs)
+#         contex['q'] = self.request.GET.get('q')
+#         return contex
